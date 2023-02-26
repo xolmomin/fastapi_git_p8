5c1a5fdf5753d6bff6656c899ee69e73893d6053
ozoda <ozodas531@gmail.com>
2023-02-26 16:18:53 +0500

    543

--- task/group4.py
+++ task/group4.py
@@ -1,5 +1,11 @@
-print('Nasrulloh')
-print('Xolmomin')
-print('Diyorbek')
-print('Ozoda')
+import csv
+import json
 
+csv_file = open('regions.csv', 'r')
+json_file = open('data.json', 'w')
+
+fieldnames = ('id', 'name')
+reader = csv.DictReader(csv_file, fieldnames)
+for row in reader:
+    json.dump(row, json_file)
+    json_file.write('\n')
