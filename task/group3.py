3f0be6007b1c8598e33d97b75ef882088c2088d5
sardor <sardorxurmatillayev2509@gmail.com>
2023-02-26 17:00:01 +0500

    COnvert csv to yaml file Sasha

--- task/group3.py
+++ task/group3.py
@@ -1,17 +1,4 @@
-import csv, json, yaml
-
-data = {}
-
-with open('regions.csv') as reg:
-    csvReader = csv.DictReader(reg)
-    for rows in csvReader:
-        print(rows)
-        key = rows['\ufeffid']
-        data[key] = rows
-
-with open('group3.json', 'w') as jsonF:
-    jsonF.write(json.dumps(data, indent=4))
-
+import csv, yaml
 
 data_y = []
 with open('regions.csv') as reg:
