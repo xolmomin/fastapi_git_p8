691fb2670e1dfdda6b14fd1a10bb4cbd8544de41
nasruloh200 <aaaaamunim@gmail.com>
2023-02-26 16:27:08 +0500

    add to group4.py

--- task/group4.py
+++ task/group4.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import yaml
 
 csv_file = open('regions_group4.csv', 'r')
 json_file = open('group4.json', 'w')
@@ -9,3 +10,12 @@ reader = csv.DictReader(csv_file, fieldnames)
 for row in reader:
     json.dump(row, json_file)
     json_file.write('\n')
+
+
+csv_file = open('regions_group4.csv', 'r')
+yaml_file = open('group4.yaml', 'w')
+
+fieldnames = ('id', 'name')
+reader = csv.DictReader(csv_file, fieldnames)
+for row in reader:
+    yaml.dump(row, yaml_file, default_flow_style=False)
