9cc7db0c963de48347b6e1f4b26fadb4810ac1c0
Abdurashid <abdurashiddido@gmail.com>
2023-02-26 16:24:16 +0500

    add group3.py

--- task/group3.py
+++ task/group3.py
@@ -1,3 +1,13 @@
-print('Abdurashid')
-print('Sardor')
-print("Nurali")
+import csv, json
+
+data = {}
+
+with open('regions.csv') as reg:
+    csvReader = csv.DictReader(reg)
+    for rows in csvReader:
+        print(rows)
+        key = rows['\ufeffid']
+        data[key] = rows
+
+with open('group3.json', 'a') as jsonF:
+    jsonF.write(json.dumps(data, indent=4))
