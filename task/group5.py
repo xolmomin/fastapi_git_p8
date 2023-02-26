e05e74e83c2f4bfafdf304859bd2d977f72a2d39
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-02-26 16:20:20 +0500

    add write to yaml

--- task/group5.py
+++ task/group5.py
@@ -1,14 +1,20 @@
-print("Ravshanjon")
-print("Nusratullo")
-print("Dilshod")
-
 import csv
 import json
+import yaml
+
+data = []
+with open('regions.csv', 'r') as f:
+    f.readline()
+    for row in csv.reader(f):
+        data.append(row)
+with open(r'group5.yaml', 'w') as file:
+    documents = yaml.dump(data, file)
+
 result = []
 with open('districts.csv') as file:
     data = csv.reader(file)
     for i in data:
         result.append(i)
 
-with open('districts.json','w') as f:
-    json.dump(result,f,indent=2)
+with open('districts.json', 'w') as f:
+    json.dump(result, f, indent=2)
