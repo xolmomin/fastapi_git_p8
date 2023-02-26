0e8c6350a4433b3db6477ca099017513c9be792d
Shokhrukh <shokhrukh.rakhmatov1234@gmail.com>
2023-02-26 16:53:10 +0500

    group3n

--- task/group3.py
+++ task/group3.py
@@ -1,3 +1,18 @@
-print('Abdurashid')
-print('Sardor')
-print("Nurali")
+import csv
+filename = 'regions.csv'
+def convert_row(headers, row):
+    s = f'<row id="{row[0]}">\n'
+    for header, item in zip(headers, row):
+        s += f'<{header}>' + f'{item}'
+    return s + '</row>'
+with open(filename, 'r') as f:
+    r = csv.reader(f)
+    headers = next(r)
+    xml = '<data>\n'
+    for row in r:
+        xml += convert_row(headers, row) + '\n'
+    xml += '</data>'
+
+
+with open('grouop3.xml', 'w') as f:
+    f.write(xml)
