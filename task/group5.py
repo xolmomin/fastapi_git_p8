3b7b7ce29d3b6a311cbe00dc7ae75a7307331a33
dilshod <absaitovdev@gmail.com>
2023-02-26 16:42:01 +0500

    ready xml

--- task/group5.py
+++ task/group5.py
@@ -1,24 +1,27 @@
+
 import csv
-import json
-import yaml
+data = []
+with open("regions.csv", 'r') as file:
 
-l=[]
-with open('regions.csv', 'r') as f:
-    f.readline()
-    for row in csv.reader(f):
-        data = {}
-        data['id']=row[0]
-        data['name']=row[1]
-        l.append(data)
-with open(r'group5.yaml', 'w') as file:
-    documents = yaml.dump(l, file)
+  csvreader = csv.reader(file)
 
-result = {}
-with open('regions.csv') as file:
-    file.readline()
-    data = csv.reader(file)
-    for i in data:
-        result[i[0]]=i[1]
+  for i in csvreader:
+    d = {"id" : i[0],
+         "name" : i[1]}
+    data.append(d)
 
-with open('group5.json','w') as f:
-    json.dump(result,f,indent=2)
+import xml.etree.ElementTree as gfg
+
+root = gfg.Element("regions")
+
+
+for i in data:
+
+  b1 = gfg.SubElement(root, "id")
+  b1.text = i.get('id')
+  b2 = gfg.SubElement(root, "name")
+  b2.text = i.get("name")
+
+tree = gfg.ElementTree(root)
+with open("group5.xml" , 'wb') as file:
+  tree.write(file)
