6848902dd40b11d8b7dc96cdf618cf6c305bb2eb
sardor <sardorxurmatillayev2509@gmail.com>
2023-02-26 16:57:37 +0500

    add yaml

--- task/group3.py
+++ task/group3.py
@@ -22,20 +22,3 @@ with open('regions.csv') as reg:
 with open('group3.yaml', 'w') as yamlF:
     yaml.dump(data_y, yamlF)
 
-filename = 'regions.csv'
-def convert_row(headers, row):
-    s = f'<row id="{row[0]}">\n'
-    for header, item in zip(headers, row):
-        s += f'<{header}>' + f'{item}'
-    return s + '</row>'
-with open(filename, 'r') as f:
-    r = csv.reader(f)
-    headers = next(r)
-    xml = '<data>\n'
-    for row in r:
-        xml += convert_row(headers, row) + '\n'
-    xml += '</data>'
-
-
-with open('grouop3.xml', 'w') as f:
-    f.write(xml)
