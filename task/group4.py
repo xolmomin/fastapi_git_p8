ff9a64c6872184934da4eb09b0a699a2dacdefa1
diordev <diordev@icloud.com>
2023-02-26 17:11:17 +0500

    group4

--- task/group4.py
+++ task/group4.py
@@ -1,32 +1,9 @@
 import csv
-import json
 import yaml
 from xml.etree import ElementTree as ET
 
 csv_file = open('regions_group4.csv', 'r')
-
-fieldnames = ('id', 'name')
-reader = csv.DictReader(csv_file, fieldnames)
-data = []
-for row in reader:
-    data.append(row)
-
-with open('group4.json', "w") as f:
-    json.dump(data, f, indent=3)
-
-
-
-csv_file = open('regions_group4.csv', 'r')
-yaml_file = open('group4.yaml', 'w')
-
-fieldnames = ('id', 'name')
-reader = csv.DictReader(csv_file, fieldnames)
-for row in reader:
-    yaml.dump(row, yaml_file, default_flow_style=False)
-
-
-csv_file = open('regions_group4.csv', 'r')
-xml_file = open('group4.xml', 'w')
+csv_file.readline()
 
 fieldnames = ('id', 'name')
 reader = csv.DictReader(csv_file, fieldnames)
@@ -34,8 +11,24 @@ reader = csv.DictReader(csv_file, fieldnames)
 root = ET.Element('data')
 for row in reader:
     item = ET.SubElement(root, 'item')
-    for key, value in row.items():
-        child = ET.SubElement(item, key)
-        child.text = value
+    id_elem = ET.SubElement(item, 'id')
+    id_elem.text = row['id']
+    name_elem = ET.SubElement(item, 'name')
+    name_elem.text = row['name']
 
-xml_file.write(ET.tostring(root).decode())
+xml_data = ET.tostring(root)
+
+with open('group4.xml', mode='wb') as xml_file:
+    xml_file.write(xml_data)
+
+    csv_file = open('regions_group4.csv', 'r')
+    csv_file.readline()
+    fieldnames = ('id', 'name')
+    reader = csv.DictReader(csv_file, fieldnames)
+    data = {'data': []}
+    for row in reader:
+        data['data'].append({'id': row['id'], 'name': row['name']})
+
+    yaml_data = yaml.dump(data)
+    with open('group4.yaml', mode='w') as yaml_file:
+        yaml_file.write(yaml_data)
