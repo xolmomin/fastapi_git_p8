0a20e4efb2760fdc37c71d87c06f2e3ffe767030
zayniddin <zayniddin2909@gmail.com>
2023-02-26 16:49:11 +0500

    add group1.yaml

--- task/group1.py
+++ task/group1.py
@@ -2,16 +2,11 @@ import csv
 import json
 import yaml
 
-with open('regions.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-
-    json_data = list(reader)
-
-with open('group1.json', 'w') as jsonfile:
-    json.dump(json_data, jsonfile, indent=4)
 
 with open("group1.json") as file:
     s = json.load(file)
 
 with open("group1.yaml", "w") as file:
     yaml.dump(s, file)
+
+
