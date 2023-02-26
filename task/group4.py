abeed2eb66b3d6ec737e1b97c0076c5e79b7b66c
diordev <diordev@icloud.com>
2023-02-26 16:18:30 +0500

    1234

--- task/group4.py
+++ task/group4.py
@@ -3,3 +3,20 @@ print('Xolmomin')
 print('Diyorbek')
 print('Ozoda')
 
+import csv
+from xml.etree import ElementTree as ET
+
+csv_file = open('regions_group4.csv', 'r')
+xml_file = open('group4.xml', 'w')
+
+fieldnames = ('id', 'name')
+reader = csv.DictReader(csv_file, fieldnames)
+
+root = ET.Element('data')
+for row in reader:
+    item = ET.SubElement(root, 'item')
+    for key, value in row.items():
+        child = ET.SubElement(item, key)
+        child.text = value
+
+xml_file.write(ET.tostring(root).decode())
