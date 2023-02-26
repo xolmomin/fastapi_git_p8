e581d466d036744509b7ab40874b0866a1af3dca
ozoda <ozodas531@gmail.com>
2023-02-26 16:24:39 +0500

    543

--- task/group4.py
+++ task/group4.py
@@ -1,22 +1,11 @@
 import csv
 import json
 
-import csv
-from xml.etree import ElementTree as ET
-
 csv_file = open('regions_group4.csv', 'r')
-xml_file = open('group4.xml', 'w')
-csv_file = open('regions.csv', 'r')
-json_file = open('data.json', 'w')
+json_file = open('group4.json', 'w')
 
 fieldnames = ('id', 'name')
 reader = csv.DictReader(csv_file, fieldnames)
-
-root = ET.Element('data')
 for row in reader:
-    item = ET.SubElement(root, 'item')
-    for key, value in row.items():
-        child = ET.SubElement(item, key)
-        child.text = value
-
-xml_file.write(ET.tostring(root).decode())
+    json.dump(row, json_file)
+    json_file.write('\n')
