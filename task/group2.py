25420c3b7f4d761fc2ecfe7272c47f111bd2b18d
khasanjon <khasanjonmamadaliyev.eng@gmail.com>
2023-02-26 16:01:57 +0500

    add to python code print('khasanjon')

--- task/group2.py
+++ task/group2.py
@@ -1,10 +1,6 @@
-<<<<<<<<< Temporary merge branch 1
 print('Xolmomin')
-=========
-print("Dilshod")
 print("Ravshanjon")
 print("Sharofiddin1")
 print("Nusratullo")
 print('Rahmiddin')
 print('Khasanjon')
->>>>>>>>> Temporary merge branch 2
