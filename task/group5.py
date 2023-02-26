6d85cb3e0442bde2511ad8399e302abf7e194f01
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-02-26 16:40:46 +0500

    pip8

--- task/group5.py
+++ task/group5.py
@@ -2,13 +2,13 @@ import csv
 import json
 import yaml
 
-l=[]
+l = []
 with open('regions.csv', 'r') as f:
     f.readline()
     for row in csv.reader(f):
         data = {}
-        data['id']=row[0]
-        data['name']=row[1]
+        data['id'] = row[0]
+        data['name'] = row[1]
         l.append(data)
 with open(r'group5.yaml', 'w') as file:
     documents = yaml.dump(l, file)
@@ -18,7 +18,7 @@ with open('regions.csv') as file:
     file.readline()
     data = csv.reader(file)
     for i in data:
-        result[i[0]]=i[1]
+        result[i[0]] = i[1]
 
-with open('group5.json','w') as f:
-    json.dump(result,f,indent=2)
+with open('group5.json', 'w') as f:
+    json.dump(result, f, indent=2)
