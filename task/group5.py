5143cde699f1f7c1f9f1e67183746201984b4663
nusratullo2004 <122892486+nusratullo2004@users.noreply.github.com>
2023-02-26 16:14:59 +0500

    nusratullo

--- task/group5.py
+++ task/group5.py
@@ -1,3 +1,14 @@
 print("Ravshanjon")
 print("Nusratullo")
 print("Dilshod")
+
+import csv
+import json
+result = []
+with open('districts.csv') as file:
+    data = csv.reader(file)
+    for i in data:
+        result.append(i)
+
+with open('districts.json','w') as f:
+    json.dump(result,f,indent=2)
