d8a0f0dd0d28f232c0dc5623a77c2a864f864f13
Abdurashid <abdurashiddido@gmail.com>
2023-02-26 16:51:31 +0500

    add group3.py

--- task/group3.py
+++ task/group3.py
@@ -1,32 +1,24 @@
-import csv, yaml
+import csv, json, yaml
 
-data = []
+data = {}
 
 with open('regions.csv') as reg:
     csvReader = csv.DictReader(reg)
     for rows in csvReader:
-        data.append(rows)
+        print(rows)
+        key = rows['\ufeffid']
+        data[key] = rows
 
-with open('group3.yaml', 'a') as yamlF:
-    yaml.dump(data, yamlF)
-#
-# data = csv.reader(csv_file, delimiter=',', quotechar='"')
-# d = []
-# for index, row in enumerate(data):
-#     if index == 0:
-#         d = row
-#     else:
-#         new_yaml = open('group3.yaml', 'w')
-#         yaml_t = ""
-#         for index, raw in enumerate(row):
-#             ls = "   "
-#             raw_head = d[index].lower().replace(" ", "_").replace("-", "")
-#             if (raw_head == "source"):
-#                 lineSeperator = '  - '
-#
-#             cell_text = ls + raw_head + " : " + raw.replace(" ", ", ") + " "
-#
-#             yaml_t += cell_text
-#         print(yaml.dump(yaml_t))
+with open('group3.json', 'w') as jsonF:
+    jsonF.write(json.dumps(data, indent=4))
 
+
+data_y = []
+with open('regions.csv') as reg:
+    csvReader = csv.DictReader(reg)
+    for rows in csvReader:
+        data_y.append(rows)
+
+with open('group3.yaml', 'w') as yamlF:
+    yaml.dump(data_y, yamlF)
 
