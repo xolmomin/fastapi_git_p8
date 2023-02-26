1e303e5af7b30bea9da4ac7a97ed3d657f65bd6e
javokhir <inboxjavohir@gmail.com>
2023-02-26 16:21:52 +0500

    add print

--- task/group1.py
+++ task/group1.py
@@ -1,3 +1,15 @@
-print('Zayniddin')
-print('nuriddin')
-print('javohir')
+import csv
+import json
+
+
+with open('regions.csv', newline='') as csvfile:
+
+    reader = csv.DictReader(csvfile)
+
+    json_data = list(reader)
+
+
+with open('group1.json', 'w') as jsonfile:
+    json.dump(json_data, jsonfile, indent=4)
+
+
