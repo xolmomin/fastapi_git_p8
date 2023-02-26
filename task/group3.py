5c3e1b829247f3cf96bb9ab59faab0022788f44f
sardor <sardorxurmatillayev2509@gmail.com>
2023-02-26 16:31:38 +0500

    nurali aka ochdi

--- task/group3.py
+++ task/group3.py
@@ -1,18 +1,13 @@
-import csv
-filename = 'regions.csv'
-def convert_row(headers, row):
-    s = f'<row id="{row[0]}">\n'
-    for header, item in zip(headers, row):
-        s += f'<{header}>' + f'{item}'
-    return s + '</row>'
-with open(filename, 'r') as f:
-    r = csv.reader(f)
-    headers = next(r)
-    xml = '<data>\n'
-    for row in r:
-        xml += convert_row(headers, row) + '\n'
-    xml += '</data>'
+import csv, json, yaml
 
+data = {}
 
-with open('grouop3.xml', 'w') as f:
-    f.write(xml)
+with open('regions.csv') as reg:
+    csvReader = csv.DictReader(reg)
+    for rows in csvReader:
+        print(rows)
+        key = rows['\ufeffid']
+        data[key] = rows
+
+with open('group3.json', 'w') as jsonF:
+    jsonF.write(json.dumps(data, indent=4))
