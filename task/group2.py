03bec000424b28ac3aa4779d1a7c2c47be9119f0
sharofiddin <cyberpancher10@gmail>
2023-02-26 16:44:25 +0500

    add gruop2.yaml

--- task/group2.py
+++ task/group2.py
@@ -1,3 +1,16 @@
-print("Sharofiddin1")
-print('Rahmiddin')
-print('Khasanjon')
+import csv
+import pandas
+import yaml
+
+""" Reading whole csv file with panda library """
+df = pandas.read_csv('regions.csv')
+
+""" Dump DataFrame into getData.yml as yaml code """
+with open('group2.yaml', 'w') as outfile:
+    yaml.dump(
+        df.to_dict(orient='records'),
+        outfile,
+        sort_keys=False,
+        width=72,
+        indent=4
+    )
